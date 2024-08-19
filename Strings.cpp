f921543e9f597c8c0646d94ce7affe6bb8303278
AthaCro <lordoftimuria@yandex.ru>
2024-08-19 18:57:32 +0300

    add tmp

--- Strings.cpp
+++ Strings.cpp
@@ -8,8 +8,10 @@ int main()
 	std::string x = "Hello, Artyom!";
 	std::cout << x << "\n";
 	std::cout << x.length() << "\n";
-	std::cout << "H\n";
-	std::cout << "!\n";
+	
+	std::string tmp = x;
+	std::cout << tmp[0] << "\n";
+	std::cout << tmp[13];
 	std::getline(std::cin, x);
 		
 	
